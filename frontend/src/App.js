f919f04140a6eb042a99cc491ebdebe93a5a5a99
UmimChoi <umimchoi@gmail.com>
2021-06-02 10:07:15 +0700

    install packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef, useState } from "react"
+// import Peer from "simple-peer"
+import io from "socket.io-client"
 
+const socket = io.connect('http://localhost:5000')
 function App() {
 
   const myVideo = useRef()
   const userVideo = useRef()
   const connectionRef = useRef()
-
+  const [ stream, setStream ] = useState()
+/*
   useEffect(()=> {
     navigator.mediaDevices.getUserMedia({video: true,audio: true}).then((stream) =>{
       setStream(stream)
@@ -14,7 +18,7 @@ function App() {
     })
 
   },[])
-  
+  */ 
   //อยู่ใน const callUser กับ const answerCall จ้า
   /*peer.on("stream", (stream) => {
     userVideo.current.srcObject = stream
@@ -23,16 +27,18 @@ function App() {
 
   return (
     <div className="container">
+      <p>test</p>
       <div className="video-container">
         <div className="video">
           {stream && <video playsInline muted ref={myVideo} autoPlay style={{width: "300px"}}/>}
         </div>
-        <div className="video">
-          {callAccepted && !callEnded ?
+      {/*  <div className="video">
+         {callAccepted && !callEnded ?
             <video playsInline ref={userVideo} autoPlay style={{width:"300px"}}/>:
-          null}
-        </div>
-      </div>
+        null} 
+        </div> 
+         */}
+      </div> 
     </div>
   );
 }
