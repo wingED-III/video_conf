cf6f52b1917e78b078738b52e9cc2830e31161f8
UmimChoi <umimchoi@gmail.com>
2021-06-02 10:26:23 +0700

    add route & history

---
+++ frontend/src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import history from "./history";
+function Routes() {
+    return (
+        <Router history={history}>
+      <Switch>{/*
+        <Route
+          path="/"
+          exact
+          component={}
+        />
+*/}
+
+      </Switch>
+    </Router>
+    )
+}
+
+export default Routes
